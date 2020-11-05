bb1f5fb8d3b1aefdc43a7a34a6547ecf796a4da8
sansan <sandikoprastyo@gmail.com>
2020-11-05 21:55:09 +0700

    nodejs express heroku websocket/scoket.io

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const INDEX = './index.html';
 const PORT = process.env.PORT || 3000;
+const socketIO = require('socket.io');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -13,7 +14,7 @@ const server = express()
   });
 
 //server websocket
-const { Server } = require('ws');
+/* const { Server } = require('ws');
 const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
@@ -26,3 +27,14 @@ setInterval(() => {
     client.send(new Date().toTimeString());
   });
 }, 1000);
+ */
+
+//server socket.io
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnect'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
